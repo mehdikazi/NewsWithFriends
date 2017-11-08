5e8c4fb3ebe3f4c98732ae60ebc74852df8d625d
Mehdi <mehdimkazi@gmail.com>
2017-11-08 05:40:45 -0800

    Cleaned up large stat page

--- SmallStats.js
+++ SmallStats.js
@@ -6,11 +6,34 @@ class SmallStats extends React.Component {
 		this.state = {
 			reactionSet: ["like", "love", "haha", "wow", "sad", "angry"]
 		}
+		this.makeUL = this.makeUL.bind(this);
+	}
+
+	makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+				console.log(array[i]);
+        item.appendChild(document.createTextNode(array[i]));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+		console.log(list);
+    return list;
 	}
 
 	render() {
 		const currState = this.props.appState;
-		console.log(currState);
+		const currentUser =
+		console.log(currState[currState.reactionSelected]);
 		const fileName = "images/" + currState.reactionSelected + ".svg";
 		const nextIndex = (this.state.reactionSet.indexOf(currState.reactionSelected) + 1) % 6
 		var prevIndex = (this.state.reactionSet.indexOf(currState.reactionSelected) - 1) % 6
@@ -21,39 +44,100 @@ class SmallStats extends React.Component {
 		console.log("curr: " + this.state.reactionSet.indexOf(currState.reactionSelected) + " next: " + nextIndex + " prev: " + prevIndex)
 			if (currState.reactionSelected) {
 				return(
-					<div>
-						<div>
-							<img src={fileName} style={{
-								width:100,
-								height:100
-							}}/>
+					<div style={{
+						borderColor: 'black',
+						borderWidth: 2,
+						borderRadius: 4,
+						borderStyle: 'solid',
+						width: "50%",
+						padding: 24,
+						margin: '0px auto',
+					}}>
+						<div style={{
+							fontWeight: 'bold',
+							fontSize: 32,
+							display: 'flex',
+							justifyContent: 'center',
+							marginBottom: 40,
+							width: '70%',
+							height: '100%',
+							margin: 'auto',
+							marginTop: '2%',
+							fontSize: 32,
+							fontFamily: 'Georgia',
+							letterSpacing: '0.05em',
+							lineHeight: '1.5em',
+						}}>
+							{currState.reactionSelected.toUpperCase()}
 						</div>
-						<div>
-							{currState.reactionSelected}
-						</div>
-						<div>
-							<img src="images/back.svg" style={{
-								width:50,
-								height:50
-							}}
+						<div style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+							marginTop: 24,
+							marginBottom: 24,
+						}}>
+							<div>
+								<img src="images/back.svg" style={{
+									width:50,
+									height:50,
+								}}
+								onClick={() => {this.props.onClickStatReactions(this.state.reactionSet[prevIndex])}}
+								/>
+							</div>
 
-							onClick={() => {this.props.onClickStatReactions(this.state.reactionSet[prevIndex])}}
-							/>
-							<img src="images/next.svg" style={{
-								width:50,
-								height:50
-							}}
-							onClick={() => {this.props.onClickStatReactions(this.state.reactionSet[nextIndex])}}
-							/>
+							<div style={{
+								margin: '0px 24px',
+							}}>
+								<img src={fileName}
+									style={{
+										width:100,
+										height:100
+									}}/>
+							</div>
+
+							<div>
+								<img src="images/next.svg" style={{
+									width:50,
+									height:50,
+								}}
+								onClick={() => {this.props.onClickStatReactions(this.state.reactionSet[prevIndex])}}
+								/>
+							</div>
 						</div>
-						<div>
-							Me: {currState[currState.reactionSelected]}
-						<div>
-						</div>
-							Mehdi: {currState.centerUser[currState.reactionSelected]}
-						</div>
-						<div>
-							Kamgucci: {currState.rightUser[currState.reactionSelected]}
+						<div style={{
+							width: '70%',
+							margin: '0px auto',
+							fontSize: 20,
+							fontFamily: 'Georgia',
+						}}>
+							<div>
+								<strong>Me:</strong> <a target="_blank" href="https://www.huffingtonpost.com/entry/syria-paris-agreement_us_5a01b87fe4b0368a4e86ed4d">Article By Huffingtonpost</a>
+								<ul>
+									<li>{currState[currState.reactionSelected].map(function(listValue){
+											return <li>{listValue}</li>;
+										})}
+									</li>
+								</ul>
+							</div>
+							<div>
+								<strong>Rochelle:</strong> <a target="_blank" href="http://www.bbc.com/news/world-middle-east-41904650">Article By BBC</a>
+								<ul>
+									<li>{currState.centerUser[currState.reactionSelected].map(function(listValue){
+											return <li>{listValue}</li>;
+										})}
+									</li>
+								</ul>
+							</div>
+							<div>
+								<strong>Jessica:</strong> <a target="_blank" href="http://www.washingtonexaminer.com/syria-joins-paris-climate-deal-leaving-us-as-the-only-country-opposed/article/2639852">Article By Washington Examiner</a>
+								<ul>
+									<li>{currState.rightUser[currState.reactionSelected].map(function(listValue){
+											return <li>{listValue}</li>;
+										})}
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				);
@@ -76,7 +160,7 @@ class SmallStats extends React.Component {
 							letterSpacing: '0.05em',
 							lineHeight: '1.5em',
 						}}>
-						React Reactions!
+						React Reactions
 						</div>
 						<div style={{
 							textIndent: 50,
