1d76d214defa5c741d46f64a4afd24b93cf3bb14
alexkamgar <alexkamgar@berkeley.edu>
2017-11-07 23:03:06 -0800

    got count and written list working

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
      this.state = {
        selected: '',
        isOpen: false,
-       like: [],
+       like: ["test", "temp"],
        love: [],
        haha: [],
        wow: [],
@@ -22,6 +22,7 @@ class App extends React.Component {
      this.getReactions = this.getReactions.bind(this);
      this.openPopover = this.openPopover.bind(this);
      this.apiCall = this.apiCall.bind(this);
+     // this.getLikeCount = this.getLikeCount.bind(this);
    }
 
   selectText() {
@@ -56,6 +57,10 @@ class App extends React.Component {
      this.setState({
        isOpen: !this.state.isOpen,
      });
+   }
+
+   getLikeCount() {
+    return this.state.like.length
    }
 
    getReactions() {
@@ -104,15 +109,15 @@ class App extends React.Component {
    }
 
    render() {
-      if (true) {
+      if (false) {
         return (
           <div>
-            like
-            love
-            haha
-            wow
-            sad
-            angry
+            like {this.state.like} count: {this.getLikeCount()}
+            love {this.state.love}
+            haha {this.state.haha}
+            wow {this.state.wow}
+            sad {this.state.sad}
+            angry {this.state.angry}
           </div>
         );
       }
@@ -130,12 +135,12 @@ class App extends React.Component {
              fontFamily: 'Georgia',
              letterSpacing: '0.05em',
              lineHeight: '1.5em',
-         }}>
+          }}>
              {this.openPopover()}
              <div style={{
                textIndent: 50,
              }}>
-               Sometimes Dustin uses psychic powers around Netflix subscribers near an Eggo. Sometimes Tommy hides over scientists around chocolate pudding.
+               {this.state.like} Sometimes Dustin uses psychic powers around Netflix subscribers near an Eggo. Sometimes Tommy hides over scientists around chocolate pudding.
                Trail mix deciphers Christmas lights despite the fact that Baby Holly freaks out about unwitting neighbors nearby. A walkie talkie stands up for the little guy while Deputy Powell defends the honor of Nutty Bars. Nancy questions the sanity of Baby Holly even though Mike crawls out of a portal in a tree with a bike. A ham radio flips a van using telekinesis while Officer Callahan desparately searches for middle school students. A walkie talkie uses an axe on the wall despite the fact that Becky Ives freaks out about soldiers nearby. Barb wears a wig while across Hawkins Middle School, supermarket patrons demand justice for Barb under a Millennium Falcon model. However, Nancy runs on a Dungeons & Dragons campaign in Bazooka Bubble Gum. However, Will deciphers Christmas lights near The Clash over Jonathan's camera. Surprisingly, comic books crawl out of a portal in a tree under Jonathan's camera. Mr. Clarke questions the sanity of Steve even though Scott stands up for the little guy over a bike. A ham radio casts a Spell of Protection despite the fact that Jonathan runs from police station donuts nearby. Netflix subscribers question the sanity of Will, ingorning that Connie Frazier demands justice for Barb. It's always a treat when Troy stands up for the little guy in a flea and an acrobat on a ham radio. Sometimes Dustin uses psychic powers around Netflix subscribers near an Eggo. Sometimes Tommy hides over scientists around chocolate pudding. Trail mix deciphers Christmas lights despite the fact that Baby Holly freaks out about unwitting neighbors nearby. A walkie talkie stands up for the little guy while Deputy Powell defends the honor of Nutty Bars. Nancy questions the sanity of Baby Holly even though Mike crawls out of a portal in a tree with a bike. A ham radio flips a van using telekinesis while Officer Callahan desparately searches for middle school students. A walkie talkie uses an axe on the wall despite the fact that Becky Ives freaks out about soldiers nearby. Barb wears a wig while across Hawkins Middle School, supermarket patrons demand justice for Barb under a Millennium Falcon model. However, Nancy runs on a Dungeons & Dragons campaign in Bazooka Bubble Gum. However, Will deciphers Christmas lights near The Clash over Jonathan's camera. Surprisingly, comic books crawl out of a portal in a tree under Jonathan's camera. Mr. Clarke questions the sanity of Steve even though Scott stands up for the little guy over a bike. A ham radio casts a Spell of Protection despite the fact that Jonathan runs from police station donuts nearby. Netflix subscribers question the sanity of Will, ingorning that Connie Frazier demands justice for Barb. It's always a treat when Troy stands up for the little guy in a flea and an acrobat on a ham radio.
              </div>
              <br></br>
